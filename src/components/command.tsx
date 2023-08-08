dc228945acde883917144ad21a58b5225f6dba32
markshawn2020 <shawninjuly@gmail.com>
2023-08-09 01:48:26 +0800

    :angry: fixed lucide-react

--- src/components/command.tsx
+++ src/components/command.tsx
@@ -17,46 +17,56 @@ import {
 	CommandSeparator,
 	CommandShortcut,
 } from '@/components/ui/command'
+import { Dialog, DialogContent, DialogTrigger } from '@/components/ui/dialog'
 
 export function CommandDemo() {
 	return (
-		<Command className="rounded-lg border shadow-md">
-			<CommandInput placeholder="Type a command or search..."/>
-			<CommandList>
-				<CommandEmpty>No results found.</CommandEmpty>
-				<CommandGroup heading="Suggestions">
-					<CommandItem>
-						<CalendarIcon className="mr-2 h-4 w-4"/>
-						<span>Calendar</span>
-					</CommandItem>
-					<CommandItem>
-						<FaceIcon className="mr-2 h-4 w-4"/>
-						<span>Search Emoji</span>
-					</CommandItem>
-					<CommandItem>
-						<RocketIcon className="mr-2 h-4 w-4"/>
-						<span>Launch</span>
-					</CommandItem>
-				</CommandGroup>
-				<CommandSeparator/>
-				<CommandGroup heading="Settings">
-					<CommandItem>
-						<PersonIcon className="mr-2 h-4 w-4"/>
-						<span>Profile</span>
-						<CommandShortcut>⌘P</CommandShortcut>
-					</CommandItem>
-					<CommandItem>
-						<EnvelopeClosedIcon className="mr-2 h-4 w-4"/>
-						<span>Mail</span>
-						<CommandShortcut>⌘B</CommandShortcut>
-					</CommandItem>
-					<CommandItem>
-						<GearIcon className="mr-2 h-4 w-4"/>
-						<span>Settings</span>
-						<CommandShortcut>⌘S</CommandShortcut>
-					</CommandItem>
-				</CommandGroup>
-			</CommandList>
+		<Command className="rounded-lg border shadow-md max-w-sm">
+			<Dialog>
+				<DialogTrigger asChild>
+					<CommandInput placeholder="Type a command or search..." className={''}/>
+					{/*<CommandShortcut>⌘B</CommandShortcut>*/}
+				</DialogTrigger>
+				<DialogContent>
+					<CommandInput placeholder="Type a command or search..."/>
+					<CommandList>
+						<CommandEmpty>No results found.</CommandEmpty>
+						<CommandGroup heading="Suggestions">
+							<CommandItem>
+								<CalendarIcon className="mr-2 h-4 w-4"/>
+								<span>Calendar</span>
+							</CommandItem>
+							<CommandItem>
+								<FaceIcon className="mr-2 h-4 w-4"/>
+								<span>Search Emoji</span>
+							</CommandItem>
+							<CommandItem>
+								<RocketIcon className="mr-2 h-4 w-4"/>
+								<span>Launch</span>
+							</CommandItem>
+						</CommandGroup>
+						<CommandSeparator/>
+						<CommandGroup heading="Settings">
+							<CommandItem>
+								<PersonIcon className="mr-2 h-4 w-4"/>
+								<span>Profile</span>
+								<CommandShortcut>⌘P</CommandShortcut>
+							</CommandItem>
+							<CommandItem>
+								<EnvelopeClosedIcon className="mr-2 h-4 w-4"/>
+								<span>Mail</span>
+								<CommandShortcut>⌘B</CommandShortcut>
+							</CommandItem>
+							<CommandItem>
+								<GearIcon className="mr-2 h-4 w-4"/>
+								<span>Settings</span>
+								<CommandShortcut>⌘S</CommandShortcut>
+							</CommandItem>
+						</CommandGroup>
+					</CommandList>
+				</DialogContent>
+			</Dialog>
 		</Command>
+	
 	)
 }
