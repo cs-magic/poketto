5a4b11d954f7bada384ba88e0246024e87a48b05
markshawn2020 <shawninjuly@gmail.com>
2023-08-10 18:34:08 +0800

    :sparkles: added sort order

--- src/pages/explore.tsx
+++ src/pages/explore.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import { clsx } from 'clsx'
 import { FlowgptAgentCard } from '@/components/flowgpt/agent.card'
 import { RootLayout } from '@/layouts/root.layout'
@@ -9,18 +9,20 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 import { CardsLayoutType, useStore } from '@/store'
 import { ScrollTrigger } from '@/components/scroll-trigger'
 import { SortOrder } from '@/ds/flowgpt'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import _ from 'lodash'
 // import SelectDemo from '@/components/radix-ui/select.demo'
 
 
 export const AgentsPage = () => {
 	
-	const query = api.flowgpt.listPrompts.useInfiniteQuery({ order: undefined }, {
-		getNextPageParam: (lastPage, allPages) => lastPage.nextCursor, // 这个必须加
-	})
-	
 	const { cardsLayout, setCardsLayout, order, setOrder } = useStore()
 	const Container = cardsLayout === CardsLayoutType.grid ? GridContainer : MasonryContainer
 	
+	const query = api.flowgpt.listPrompts.useInfiniteQuery({ order }, {
+		getNextPageParam: (lastPage, allPages) => lastPage.nextCursor, // 这个必须加
+	})
 	
 	return (
 		<RootLayout>
@@ -34,7 +36,7 @@ export const AgentsPage = () => {
 				{/*<HomeCarousel/>*/}
 				
 				{/* title */}
-				<div className={'flex items-center gap-2 | text-2xl text-muted-foreground '}>
+				<div className={'w-full | flex items-center gap-2 | text-2xl text-muted-foreground '}>
 					<IconArrowBadgeDownFilled/>
 					<span>玩法推荐</span>
 					
@@ -49,16 +51,33 @@ export const AgentsPage = () => {
 						</SelectContent>
 					</Select>
 					
-					<Select onValueChange={setCardsLayout}>
-						<SelectTrigger className={'w-28'}>
-							<SelectValue placeholder={'排序指标'}/>
-						</SelectTrigger>
-						<SelectContent>
-							{Object.values(SortOrder).map((cl) => (
-								<SelectItem value={cl} key={cl}>{cl}</SelectItem>
-							))}
-						</SelectContent>
-					</Select>
+					{/*<Select onValueChange={setOrder}>*/}
+					{/*	<SelectTrigger className={'w-28'}>*/}
+					{/*		<SelectValue placeholder={'排序指标'}/>*/}
+					{/*	</SelectTrigger>*/}
+					{/*	<SelectContent>*/}
+					{/*		{Object.values(SortOrder).map((cl) => (*/}
+					{/*			<SelectItem value={cl} key={cl}>{cl}</SelectItem>*/}
+					{/*		))}*/}
+					{/*	</SelectContent>*/}
+					{/*</Select>*/}
+					<div className={'grow'}/>
+					<div className={'flex items-center'}>
+						{
+							Object.values(SortOrder)
+								.map((order) => (
+									<Fragment key={order}>
+										<Separator orientation={'vertical'} className={'h-4 first:hidden'}/>
+										<Button
+											className={'hover:bg-transparent px-2'}
+											variant={'ghost'}
+											key={order} onClick={() => {setOrder(order)}}>
+											{_.startCase(_.capitalize(order))}
+										</Button>
+									</Fragment>
+								))
+						}
+					</div>
 				</div>
 				
 				{/* content (carousel - cards)*/}
