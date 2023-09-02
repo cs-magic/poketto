0fc6c4fd95307ff4163d5d1f9893128fbd80ed06
markshawn2020 <shawninjuly@gmail.com>
2023-09-02 20:48:51 +0800

    test

--- src/lib/proxy.ts
+++ src/lib/proxy.ts
@@ -1,6 +1,7 @@
 import { isDomestic } from "@/lib/router"
 
 export const createHttpAgent = async () => {
+  return undefined // todo: test clash tun mode
   if (!isDomestic()) return undefined
   const { HttpsProxyAgent } = await import("https-proxy-agent")
   return new HttpsProxyAgent("http://localhost:7890")
