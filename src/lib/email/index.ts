369d46e571a0f2c8c5943b983adb652da70d11c2
markshawn2020 <shawninjuly@gmail.com>
2023-09-07 20:48:50 +0800

    fix

--- src/lib/email/index.ts
+++ src/lib/email/index.ts
@@ -4,6 +4,7 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+import { Awaitable } from "next-auth"
 import { SendVerificationRequestParams } from "next-auth/providers"
 
 import { emailProvider } from "@/config"
@@ -16,14 +17,13 @@ export interface ISendVerificationRequest extends SendVerificationRequestParams 
   origin: string
 }
 
-export const sendVerificationRequest = async (params: ISendVerificationRequest) => {
+export const sendVerificationRequest = async (params: ISendVerificationRequest): Promise<void> => {
   console.log("sendVerificationRequest: ", params)
 
   const send = emailProvider === "aws" ? sendViaAWS : sendViaPostmark
   try {
     const result = await send(params)
     console.log({ result })
-    return result
   } catch (error) {
     console.error(error)
     throw new Error("failed to send verification request")
