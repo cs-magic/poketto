271a499a5a09f70921dcbdb3e3a5022999eeff55
南川 <xingjian@baichuan-inc.com>
2023-10-26 12:10:14 +0800

    fixed origin

--- src/lib/router.ts
+++ src/lib/router.ts
@@ -1,3 +1,4 @@
-export const getOrigin = () => (typeof window === "undefined" ? process.env.HOST! : window.location.origin)
+export const getOrigin = () =>
+  typeof window === "undefined" ? process.env.HOST ?? "http://localhost" : window.location.origin
 export const isDomestic = () => getOrigin().includes(".cn")
 export const getServerId = () => Number(isDomestic())
